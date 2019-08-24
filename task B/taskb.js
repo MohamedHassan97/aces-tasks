943ca1d0fa321eb3693702f03372c6dbf202e44a
mohamedhassan69 <54395539+mohamedhassan69@users.noreply.github.com>
2019-08-24 18:20:15 +0200

    Add files via upload

---
+++ task B/taskb.js
@@ -0,0 +1,9 @@
+var b=document.getElementById("but");
+b.addEventListener("click",function(){
+var x=document.getElementById("in").value;
+var z=document.getElementById("square");
+
+    
+    z.style.background=x;
+    
+}) ;
